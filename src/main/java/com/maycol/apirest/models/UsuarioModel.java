b184757d43756dd97717072c0c273d9c738638c0
MykeStv <mykestv22@gmail.com>
2022-02-16 14:34:53 -0500

    Package Model es creado con la clase usuario

---
+++ src/main/java/com/maycol/apirest/models/UsuarioModel.java
@@ -0,0 +1,52 @@
+package com.maycol.apirest.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usuario") //crea un tabla en la db con el nombre de usuario
+public class UsuarioModel {
+
+    //ATRIBUTES
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    private String nombre;
+    private String email;
+    private Integer prioridad;
+
+
+    //GETTER && SETTER
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getPrioridad() {
+        return prioridad;
+    }
+
+    public void setPrioridad(Integer prioridad) {
+        this.prioridad = prioridad;
+    }
+}
